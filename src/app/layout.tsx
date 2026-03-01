0cdd72d3b2768bf9ec7a8299fd027e4457a1d3b7
Sahedul Islam Rony <sahedul.dev@gmail.com>
2026-03-01 16:40:32 +0600

    feat: added vercel web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./_components/theme-provider";
 import { personalInfo } from "./_lib/data";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
